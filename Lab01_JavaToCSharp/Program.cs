b625ba4ef7aea2fc1b6d5138ff39ccd74a444191
Andrew Harkins <extremelyexceeding@gmail.com>
2018-12-17 14:49:29 -0600

    Lab01 Calculate Profit by User
    Lab01 - Console Application
    Calculate Profit by user input,
    Get user input (Number only)
    Display msg and close program if NAN Calculate selling price & profit,
    Display all data in the console.

--- Lab01_JavaToCSharp/Program.cs
+++ Lab01_JavaToCSharp/Program.cs
@@ -10,6 +10,31 @@ namespace Lab01_JavaToCSharp
     {
         static void Main(string[] args)
         {
+            decimal percentProfit = .40m;
+            decimal profitTotal = 0m;
+            decimal total = 0m;
+            string input;
+
+            // Tell user to enter the retail price.
+            Console.WriteLine("Please enter the retail price of the circuit board.");
+            // Get the users input.
+            input = Console.ReadLine();
+            // Check to make sure the user inputed a decimal.
+            if (decimal.TryParse(input, out decimal retailPrice))
+            {
+                // Calculate the selling price.
+                total = (retailPrice * percentProfit) + retailPrice;
+                // Calculate the selling profit total.
+                profitTotal = total - retailPrice;
+                // Display all the results.
+                Console.Write("Your retail price: " + retailPrice + "\n" +
+                              "Your selling price: " + total + "\n" +
+                              "Your profit earned is: " + profitTotal);
+            }
+            else // Error.
+                Console.WriteLine("User input is not a valid retail price. Must be a number.");
+            // Keep console open to read.
+            Console.Read();
         }
     }
 }
